5f0c385142648696fb4e782f8ac162b05f322c80
Breno Lemos <brenomiros@gmail.com>
2019-09-22 14:28:44 -0300

    Usando LocalStorage

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,14 @@ export default {
       return Math.round((done / total) * 100) || 0;
     }
   },
+  watch: {
+    tasks: {
+      deep: true,
+      handler() {
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      }
+    }
+  },
   methods: {
     addTask(task) {
       const sameName = t => t.name === task.name;
@@ -43,6 +51,10 @@ export default {
     toggleTaskState(i) {
       this.tasks[i].pending = !this.tasks[i].pending;
     }
+  },
+  created() {
+    const json = localStorage.getItem("tasks");
+    this.tasks = JSON.parse(json) || [];
   }
 };
 </script>
